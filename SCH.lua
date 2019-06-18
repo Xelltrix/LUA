8628277f6e8fb9859332f511892c0869aa30a8b7
Xelltrix <5940146+Xelltrix@users.noreply.github.com>
2019-06-18 10:46:55 -0400

    Update SCH.lua

--- SCH.lua
+++ SCH.lua
@@ -329,14 +329,14 @@ function init_gear_sets()
 		
 		---Magic Burst
 		sets.magic_burst = set_combine(sets.midcast['Elemental Magic'],
-		{
+		{--		Magic Burst: +42%	Magic Burst II: +10%
 			head="Peda. M.Board +3", neck="Mizu. Kubikazari", rear="Static Earring",
 			body="Merlinic Jubbah", hands="Amalric Gages +1", lring="Locus Ring", rring="Mujin Band",
 			feet="Jhakri Pigaches +2"
 		})
 		
 		sets.magic_burst.Resistant = set_combine(sets.magic_burst,
-		{
+		{--		Magic Burst: 42%	Magic Burst II: +4%
 			hands="Regal Cuffs", lear="Barkaro. Earring",
 			waist="Eschan Stone", legs="Peda. Pants +3"
 		})
@@ -371,7 +371,7 @@ function init_gear_sets()
 		sets.idle.Vagary = set_combine(sets.magic_burst, 
 		{
 			main="Akademos", sub="Niobid Strap",
-			feet="Arbatel Loafers +1"
+			waist="Hachiran-no-Obi", feet="Arbatel Loafers +1"
 		})
 
 		sets.idle.Town = set_combine(sets.idle,
@@ -609,7 +609,7 @@ function job_post_midcast(spell, action, spellMap, eventArgs)
 	elseif (spell.skill == 'Elemental Magic' or spell.english == 'Kaustra') and (spell.element == world.day_element or (spell.element == world.weather_element and get_weather_intensity() == 1)
 		or (spell.element == world.weather_element and get_weather_intensity() == 2 and world.day_element == elements.strong_to[spell.element])) then
 		-- If the element of a non-helix spell matches either day or weather with a net intensity of 1.
-		if (spell.target.distance < (7 - spell.target.model_size)) then
+		if (spell.target.distance < (15 - spell.target.model_size)) then
 			equip
 			{
 				main="Akademos", sub="Niobid Strap",
